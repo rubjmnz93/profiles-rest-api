4dea597ff451df2b71d9aed582ffb6a18277d78a
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2023-11-09 14:22:58 +0100

    Added POST, PUT, PATCH and DELETE to our API project

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-    def get(self, request, format=None) -> Response:
+    def get(self, request: Request, format=None) -> Response:
         """Returns a list of APIViews features"""
 
         an_apiview = [
@@ -17,3 +22,29 @@ class HelloApiView(APIView):
         ]
 
         return Response({"message": "Hello!", "an_apiview": an_apiview})
+
+    def post(self, request: Request) -> Response:
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello, {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request: Request, pk=None) -> Response:
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request: Request, pk=None) -> Response:
+        """Handle a partial update an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request: Request, pk=None) -> Response:
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
